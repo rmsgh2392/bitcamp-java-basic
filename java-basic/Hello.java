abbfe3e8b6eb38430f1108daae421ead3ecd7288
rmsgh2392 <rmsgh2392@daum.net>
2019-08-09 15:43:07 +0900

    190809 15:40

--- java-basic/Hello.java
+++ java-basic/Hello.java
@@ -1,6 +1,6 @@
 class Hello{
     public static void main(String[]args ){
-      System.out.print("hello ,ÀÚ¹Ù !!!");
+      System.out.print("hello ,JAVA !!!");
 
     }
 }
--- java-basic/WhatName.java
+++ java-basic/WhatName.java
@@ -1,9 +1,7 @@
-/**
- * 값을 입력받아서 출력받는 예제
- */
+
 class  WhatName{
     public static void main(String[]args){
-        System.out.println("안녕");//출력문
+        System.out.println("hi nice me to");
     }
 }
    
